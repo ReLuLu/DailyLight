f84ffdbb5618f2bcd6d089a8ec86eaa7d2353025
ReLuLu <relulu@web.de>
2018-11-27 19:59:54 +0100

    added support for sending other players to a their last checkpoint

--- src/main/java/de/relulu/DailyLight/commands/DailyCheck.java
+++ src/main/java/de/relulu/DailyLight/commands/DailyCheck.java
@@ -2,6 +2,7 @@ package de.relulu.DailyLight.commands;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -31,42 +32,103 @@ public class DailyCheck implements CommandExecutor {
 	 */
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String comname, String[] comparams) {
-		
-		if(sender instanceof Player) {
-			
-			Player p = (Player)sender;
 
-			// wenn der Spieler einen Checkpoint erreicht hat (sich also in der CP-Liste befindet)
-			if(dman.hasPlayerCheck(p.getDisplayName())) {
-				
-				p.teleport(dman.getPlayerCheck(p.getDisplayName()));
-				mh.tell(p, mh.getPrimaryColor() + "Zurück zum Checkpoint :)");
+        if (sender instanceof Player) {
 
-			// wenn er sich nicht in der CP Liste befindet...
-			} else {
+            Player p = (Player) sender;
 
-                mh.tell(p, mh.getPrimaryColor() + "Noch keinen Checkpoint erreicht...");
+            // wenn der Befehl ohne Parameter daherkommt
+            if (comparams.length < 1) {
 
-            }
+                // wenn der Spieler einen Checkpoint erreicht hat (sich also in der CP-Liste befindet)
+                if (dman.hasPlayerCheck(p.getDisplayName())) {
 
-            // wenn der Spieler dann noch Op ist...
-            if(p.isOp()) {
-                List<String> playcheckli = dman.getPlayerCheckList();
-                if(!playcheckli.isEmpty()) {
-						
-                    mh.tell(p, mh.getPrimaryColor() + "Checkpointliste: " + mh.getSecondaryFormat());
-                    for(String s : playcheckli) {
-                        mh.tell(p, mh.getPrimaryColor() + s);
+                    p.teleport(dman.getPlayerCheck(p.getDisplayName()));
+                    mh.tell(p, mh.getPrimaryColor() + "Zurück zum Checkpoint :)");
+
+                // wenn er sich nicht in der CP Liste befindet...
+                } else {
+
+                    mh.tell(p, mh.getPrimaryColor() + "Noch keinen Checkpoint erreicht...");
+                }
+
+                // wenn der Spieler Op ist, dann gib den Checkpoint-Debug aus
+                if(p.isOp()) {
+                    printCheckpointDebug(p);
+                }
+
+            // wenn der Befehl mit Parameter daherkommt
+            } else if (comparams.length == 1) {
+
+                // wenn der Spieler dann noch Op ist...
+                if (p.isOp()) {
+
+                    String targetplayername;
+                    targetplayername = comparams[0];
+                    Player targetplayer = Bukkit.getPlayerExact(targetplayername);
+
+                    // nur wenn auch wirklich ein valider targetplayer vorhanden ist
+                    if (targetplayer != null) {
+
+                        // wenn der Spieler einen Checkpoint erreicht hat (sich also in der CP-Liste befindet)
+                        if (dman.hasPlayerCheck(targetplayer.getDisplayName())) {
+
+                            // Spieler zum Checkpoint teleportieren
+                            targetplayer.teleport(dman.getPlayerCheck(targetplayer.getDisplayName()));
+                            mh.tell(p, mh.getPrimaryColor() + "Spieler "
+                                    + mh.getSecondaryFormat() + targetplayer.getDisplayName()
+                                    + mh.getPrimaryColor() + " zurück zum Checkpoint teleportiert!");
+
+                            mh.tell(targetplayer, mh.getPrimaryColor() + "Du wurdest von "
+                                    + mh.getSecondaryFormat() + p.getDisplayName()
+                                    + mh.getPrimaryColor() + " zum letzten Checkpoint teleportiert :)");
+
+                        // wenn der Spieler noch keinen Checkpoint erreicht hat
+                        } else {
+                            mh.tell(p, mh.getSecondaryFormat() + targetplayer.getDisplayName()
+                                    + mh.getPrimaryColor() + " hat noch keinen Checkpoint erreicht...");
+                        }
+
+                        // wenn der Spieler Op ist, dann gib den Checkpoint-Debug aus
+                        if(p.isOp()) {
+                            printCheckpointDebug(p);
+                        }
+
+                    // wenn der gewünschte targetplayer nicht gefunden werden konnte
+                    } else {
+
+                        mh.tell(p, mh.getPrimaryColor() + "Spieler "
+                                + mh.getSecondaryFormat() + targetplayername
+                                + mh.getPrimaryColor() + " konnte nicht gefunden werden.");
+
+                        return true;
                     }
-                } else mh.tell(p,mh.getPrimaryColor() + "Checkpointliste: " + mh.getSecondaryFormat() + "leer");
+
+                }
+
             }
 
-        // wenn der Sender kein Spieler ist...
-		} else {
-			mh.tell(sender, mh.getPrimaryColor() + "Nur ein Spieler kann diesen Befehl nutzen!");
-		}
-		
-		return true;
-	}
+            // wenn der Sender kein Spieler ist...
+            } else {
+                mh.tell(sender, mh.getPrimaryColor() + "Nur ein Spieler kann diesen Befehl nutzen!");
+            }
+
+            return true;
+        }
+
+    /**
+     * Gibt die Liste der Spieler-/Checkpointmappings aus
+     * @param p Spieler, der die Ausgabe erhält
+     */
+    private void printCheckpointDebug(Player p){
+        List<String> playcheckli = dman.getPlayerCheckList();
+        if (!playcheckli.isEmpty()) {
+
+            mh.tell(p, mh.getPrimaryColor() + "Checkpointliste: " + mh.getSecondaryFormat());
+            for (String s : playcheckli) {
+                mh.tell(p, mh.getPrimaryColor() + s);
+            }
+        } else mh.tell(p, mh.getPrimaryColor() + "Checkpointliste: " + mh.getSecondaryFormat() + "leer");
+    }
 
 }
